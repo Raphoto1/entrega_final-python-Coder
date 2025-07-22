d41c87467c059f02a9bfbc379eab6f84b6403f5c
¨Rafael <rrhhmmtt@gmail.com>
2025-07-22 18:28:40 -0500

    se plantea root y primmer separacion

---
+++ testing_core/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TestingCoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'testing_core'
--- testing_core/urls.py
+++ testing_core/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf import settings
 
+from testing_core.views import home
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('testing_core.urls')),
+    path('', home, name='home'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
